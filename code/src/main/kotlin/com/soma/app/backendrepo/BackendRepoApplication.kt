76f1ca7cde695ac38d530944578bc052a8daec66
Mohamed Bashir <mohamedbashir@Mohameds-MBP.hitronhub.home>
2023-01-09 23:12:51 -0600

    project init

---
+++ code/src/main/kotlin/com/soma/app/backendrepo/BackendRepoApplication.kt
@@ -0,0 +1,11 @@
+package com.soma.app.backendrepo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BackendRepoApplication
+
+fun main(args: Array<String>) {
+    runApplication<BackendRepoApplication>(*args)
+}
